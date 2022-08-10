b41e610bf7986d438c4509eace4a23351eb947ec
tizenmile <tizen48@gmail.com>
2022-08-10 16:05:31 +0300

    firs commit

--- src/index.js
+++ src/index.js
@@ -1,5 +1,106 @@
+import Notiflix from 'notiflix';
 import { fetchFromPixabay } from './js/fetch.js';
-function getDataFromPixabay(params) {
-    fetchFromPixabay()
-    
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+const loadBtn = document.querySelector('.load-more');
+const getForm = document.querySelector('.header-form');
+let gallery = '';
+loadBtn.style.visibility = 'hidden';
+
+function getDataFromPixabay(name, pagePagination) {
+  return fetchFromPixabay(name, pagePagination).then(function (response) {
+    if (response.status === 200) {
+      if (response.data.hits.length === 0) {
+        loadBtn.style.visibility = 'hidden';
+        Notiflix.Notify.failure(
+          'Sorry, there are no images matching your search query. Please try again.'
+        );
+      }
+      response.data.hits.forEach(element => {
+        renderImg(element);
+      });
+      return response.data.total;
+    } else {
+      console.log(response.status);
+    }
+  });
 }
+
+getForm.addEventListener('submit', pushData);
+
+function pushData(event) {
+  event.preventDefault();
+  pagePagination = 1;
+  const dataForm = new FormData(getForm);
+  const data = dataForm.get('searchQuery');
+  document.querySelector('.gallery').innerHTML = '';
+  getDataFromPixabay(data, pagePagination).then(totalFind => {
+    if (totalFind !== 0) {
+      loadBtn.style.visibility = 'visible';
+      Notiflix.Notify.success(`Hooray! We found ${totalFind} images.`);
+    } else {
+      loadBtn.style.visibility = 'hidden';
+    }
+  });
+}
+
+loadBtn.addEventListener('click', loadMore);
+function loadMore() {
+  const dataForm = new FormData(getForm);
+  const data = dataForm.get('searchQuery');
+  pagePagination++;
+  const { height: cardHeight } = document
+    .querySelector('.gallery')
+    .firstElementChild.getBoundingClientRect();
+  getDataFromPixabay(data, pagePagination);
+  setTimeout(() => {
+    window.scrollBy({
+      top: cardHeight * 2,
+      behavior: 'smooth',
+    });
+  }, 250);
+}
+
+function renderImg({
+  tags,
+  likes,
+  views,
+  comments,
+  downloads,
+  webformatURL,
+  largeImageURL,
+}) {
+  const inner = `<div class="photo-card">
+  <a class="photo-url" href="${largeImageURL}"><img src="${webformatURL}" alt="${tags}" loading="lazy" /></a>
+    <div class="info">
+      <p class="info-item">
+        <b>Likes: ${likes}</b>
+      </p>
+      <p class="info-item">
+        <b>Views: ${views}</b>
+      </p>
+      <p class="info-item">
+        <b>Comments: ${comments}</b>
+      </p>
+      <p class="info-item">
+        <b>Downloads: ${downloads}</b>
+      </p>
+    </div>
+  </div>`;
+
+  document.querySelector('.gallery').innerHTML += inner;
+  const modal = document.querySelector('.photo-card');
+  modal.addEventListener('click', event => {
+    event.preventDefault();
+  });
+  let gallery = new SimpleLightbox('.gallery a');
+  gallery.refresh();
+}
+window.addEventListener('scroll', () => {
+  if (
+    window.scrollY + window.innerHeight >=
+    document.documentElement.scrollHeight * 0.9
+  ) {
+    loadMore();
+  }
+});
--- src/js/fetch.js
+++ src/js/fetch.js
@@ -1,17 +1,23 @@
-function fetchFromPixabay(searchName) {
-  fetch(
-    `https://pixabay.com/api/?key=29059571-ec18f33066846ba93eec17670&q=${searchName}&image_type=photo&orientation=horizontal&safesearch=true`
-  )
-    .then(response => {
-      if (!response.ok) {
-        throw new Error(response.status);
-      }
-      return response.json();
+const axios = require('axios');
+const fetchFromPixabay = async (searchName, pagePagination) => {
+  const key = '29059571-ec18f33066846ba93eec17670';
+  const getData = axios
+    .get(`https://pixabay.com/api/`, {
+      params: {
+        q: searchName,
+        key: key,
+        image_type: 'photo',
+        orientation: 'horizontal',
+        safesearch: true,
+        per_page: 40,
+        page: pagePagination,
+      },
     })
-    .then(data => {
-      console.log(data);
-    })
-    .catch(error => console.log(error));
-}
+    .catch(function (error) {
+      console.log(error);
+    });
+
+  return getData;
+};
 
 export { fetchFromPixabay };
